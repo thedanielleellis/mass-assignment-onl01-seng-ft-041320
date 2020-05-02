d4285b040df1b7a137d6ef487b3b99b1857e36a2
Danielle Ellis <thedanielleellis@gmail.com>
2020-05-02 03:24:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,11 @@
+
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :heigh,
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
-
-  def initialize (attributes=nil)
-    attributes.each{|key,value|self.send("#{key}=",value)
-  end 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes=nil)
+    if attributes
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
+  end
 end
-
-
-# class Person
-#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-#   def initialize(attributes=nil)
-#     if attributes
-#       attributes.each do |k,v|
-#         self.send("#{k}=", v)
-#       end
-#     end
-#   end
-# end
